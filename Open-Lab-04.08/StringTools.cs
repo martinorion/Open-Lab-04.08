b132a22525c1281f335030e12cd68c6ca824108b
martinorion <71751917+martinorion@users.noreply.github.com>
2021-01-10 15:14:21 +0100

    Return 4 letters long string

--- Open-Lab-04.08/StringTools.cs
+++ Open-Lab-04.08/StringTools.cs
@@ -6,7 +6,12 @@ namespace Open_Lab_04._08
     {
         public string[] IsFourLetters(string[] strings)
         {
-            throw new NotImplementedException();
+            var retl = new List<string>();
+            for (int i = 0; i < strings.Length; i++)
+                if (strings[i].Length == 4)
+                    retl.Add(strings[i]);
+            string[] reta = retl.ToArray();
+            return reta;
         }
     }
 }
